4dcef5176ab62c37b46dbee1da77e2e82cd50c0b
Scott Crowder <scrowder@gmail.com>
2024-03-18 11:23:20 -0500

    Initial Commit

---
+++ APICallTutorial/APICallTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallTutorialApp.swift
+//  APICallTutorial
+//
+//  Created by SCOTT CROWDER on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct APICallTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
